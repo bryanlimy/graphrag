8a306e86dec5857047851a5590921ae7d24d2891
Bryan Li <bryanlimy@gmail.com>
2024-09-09 16:23:27 -0700

    plot similarity vs decision rate.

---
+++ examples_notebooks/compare_embeddings.py
@@ -0,0 +1,284 @@
+import asyncio
+import os
+import pickle
+from math import ceil
+from math import floor
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib.ticker import MultipleLocator
+from sklearn.metrics.pairwise import cosine_similarity
+from tqdm import tqdm
+
+
+import plot
+import utils
+from compute_community_embedding import embed_text
+
+DPI = 300
+INPUT_DIR = Path("./inputs/podcast")
+RESULT_DIR = Path("./results/dynamic")
+PLOT_DIR = Path("figures/embeddings")
+
+plot.set_font()
+
+TICK_FONTSIZE = 8
+LABEL_FONTSIZE = 9
+TITLE_FONTSIZE = 10
+
+
+def get_embeddings():
+    report_df = pd.read_parquet(INPUT_DIR / "create_final_community_reports.parquet")
+    embedding_df = pd.read_parquet(INPUT_DIR / "community_embedding.parquet")
+    return report_df, embedding_df
+
+
+def plot_pairwise_similarity(embedding_df: pd.DataFrame, filename: Path):
+    similarities = cosine_similarity(embedding_df.embedding.to_list())
+    similarities = similarities.astype(np.float32)
+    similarities = similarities[np.triu_indices_from(similarities, k=1)]
+
+    df = pd.DataFrame({"similarity": similarities})
+    figure, ax = plt.subplots(nrows=1, ncols=1, figsize=(3, 3), dpi=DPI)
+    x_ticks = np.linspace(0, 1.0, 3)
+
+    sns.histplot(
+        df,
+        x="similarity",
+        bins=40,
+        binrange=(x_ticks[0], x_ticks[-1]),
+        color="black",
+        stat="percent",
+        fill=False,
+        linewidth=1,
+        clip_on=False,
+        alpha=0.8,
+        ax=ax,
+    )
+    ax.set_xlim(x_ticks[0], x_ticks[-1])
+    ax.set_xticks(x_ticks, labels=np.round(x_ticks, 1), fontsize=9)
+    ax.set_xlabel("Cosine similarity", fontsize=10, labelpad=0)
+    y_ticks = np.linspace(0, 50, 3)
+    ax.set_ylim(y_ticks[0], y_ticks[-1])
+    ax.set_yticks(y_ticks, labels=y_ticks.astype(int), fontsize=9)
+    ax.tick_params(axis="both", which="both", length=2, pad=1, width=0.8)
+    ax.set_title("Community Embeddings\nPairwise Similarity", fontsize=10, pad=2)
+    sns.despine(ax=ax, trim=True)
+
+    filename.parent.mkdir(parents=True, exist_ok=True)
+    figure.savefig(filename, dpi=DPI, bbox_inches="tight", pad_inches=0.02)
+    plt.close(figure)
+
+
+def get_community_decisions(qid: int) -> (list[str], np.ndarray):
+    """Return decisions results in the same order as community"""
+    result_dir = RESULT_DIR / f"query{qid:03d}"
+    with open(result_dir / "result.pkl", "rb") as file:
+        result = pickle.load(file)
+
+    # extract decision results for each community that was queried
+    communities = list(result["selection_result"]["outputs"].keys())
+    decisions = []
+    for community in communities:
+        decision = list(result["selection_result"]["outputs"][community].values())
+        decision = [int(v[0]) for l in decision for v in l]
+        options, counts = np.unique(decision, return_counts=True)
+        decisions.append(options[np.argmax(counts)])
+    # communities = np.array(communities, dtype=int)
+    decisions = np.array(decisions, dtype=int)
+    # communities = report_df.loc[report_df["level"] == 0]["community"].to_list()
+    # assert np.array_equal(
+    #     communities,
+    #     list(result["selection_result"]["outputs"].keys())[: len(communities)],
+    # )
+    # decisions = result["selection_result"]["decisions"][: len(communities)]
+    # decisions = np.array([int(d) for d in decisions], dtype=int)
+    return communities, decisions
+
+
+def plot_community_decision(
+    similarities: np.ndarray,
+    decisions: np.ndarray,
+    filename: Path,
+    qid: int = None,
+    title: str = None,
+    scale_width: bool = True,
+    add_p_value: bool = False,
+):
+    options = [1, 2, 3, 4, 5]
+    data, widths = [], []
+    for decision in options:
+        idx = np.where(decisions == decision)[0]
+        if len(idx) == 0:
+            data.append([])
+        else:
+            data.append(similarities[idx])
+        widths.append((len(idx) / len(decisions)) + 0.1)
+
+    figure, ax = plt.subplots(nrows=1, ncols=1, figsize=(3, 2.5), dpi=DPI)
+
+    x_ticks = np.arange(len(options), dtype=int)
+    y_ticks = np.linspace(
+        0.1 * floor(10 * np.min(similarities)),
+        0.1 * ceil(10 * np.max(similarities)),
+        num=3,
+    )
+
+    bp = ax.boxplot(
+        data,
+        # notch=True,
+        vert=True,
+        positions=x_ticks,
+        widths=widths if scale_width else 0.4,
+        showfliers=False,
+        flierprops={"marker": "o", "markersize": 2, "alpha": 0.5},
+        capprops={"clip_on": False},
+        whiskerprops={"clip_on": False},
+        medianprops={
+            "color": "red",
+            # "zorder": 1,
+            "solid_capstyle": "projecting",
+        },
+    )
+
+    scatter_kw = {
+        "s": 7,
+        "marker": ".",
+        "alpha": 0.3,
+        "zorder": 0,
+        "facecolors": "none",
+        "edgecolors": "dodgerblue",
+        "clip_on": False,
+    }
+    for i, option in enumerate(options):
+        idx = np.where(decisions == option)[0]
+        y = similarities[idx]
+        x = np.random.normal(i, 0.06, size=len(y))
+        ax.scatter(x, y, **scatter_kw)
+
+    if add_p_value:
+        max_height = max(np.max(data[0]), np.max(data[1])) + 0.01
+        for i in [1, 2, 3, 4]:
+            text_height = plot.add_p_value(
+                ax,
+                x0=x_ticks[0],
+                x1=x_ticks[i],
+                y=max_height,
+                y_offset=0.002,
+                array1=data[0],
+                array2=data[i],
+                fontsize=TICK_FONTSIZE,
+            )
+            max_height = max(max_height, text_height) + 0.015
+
+    ax.set_xlim(x_ticks[0] - 0.6, x_ticks[-1] + 0.6)
+    plot.set_xticks(
+        ax,
+        ticks=x_ticks,
+        tick_labels=options,
+        label="Decision",
+        tick_fontsize=TICK_FONTSIZE,
+        label_fontsize=LABEL_FONTSIZE,
+        label_pad=0,
+    )
+
+    ax.set_ylim(y_ticks[0], y_ticks[-1])
+    plot.set_yticks(
+        ax,
+        ticks=y_ticks,
+        tick_labels=[f"{y:.2f}" for y in y_ticks],
+        label="Cosine similarity",
+        tick_fontsize=TICK_FONTSIZE,
+        label_fontsize=LABEL_FONTSIZE,
+        label_pad=0,
+    )
+    # ax.yaxis.set_minor_locator(MultipleLocator(0.01))
+
+    plot.set_ticks_params(ax, length=2)
+    ax.tick_params(axis="x", which="major", length=0)
+    sns.despine(ax=ax, bottom=True, trim=True)
+
+    if title is None:
+        title = f"Cosine similarity between\nquery {qid} and community embeddings"
+    ax.set_title(title, fontsize=LABEL_FONTSIZE, pad=6, linespacing=1)
+
+    figure.tight_layout()
+    plot.save_figure(figure, filename=filename, dpi=4 * DPI)
+
+
+def compare_community_embedding_with_query(
+    qid: int,
+    embedding_df: pd.DataFrame,
+):
+    result_dir = RESULT_DIR / f"query{qid:03d}"
+    if not result_dir.exists():
+        return None, None
+
+    # get communities that were queried and their decisions
+    communities, decisions = get_community_decisions(qid)
+
+    query_embedding = embed_text(utils.QUERIES[qid])
+
+    # get community embeddings in the order of decisions
+    community_embeddings = []
+    for community in communities:
+        embedding = embedding_df.loc[embedding_df["community"] == community]
+        community_embeddings.append(embedding.iloc[0].embedding.astype(np.float32))
+
+    # compare similarity between query and each community
+    similarities = cosine_similarity([query_embedding] + community_embeddings)
+    similarities = similarities[0, 1:].astype(np.float32)
+
+    plot_community_decision(
+        similarities=similarities,
+        decisions=decisions,
+        filename=PLOT_DIR / f"query{qid:03d}_community_decision.jpg",
+        qid=qid,
+    )
+
+    return similarities, decisions
+
+
+def main():
+    report_df, embedding_df = get_embeddings()
+
+    plot_pairwise_similarity(
+        embedding_df,
+        filename=PLOT_DIR / "pairwise_similarity.jpg",
+    )
+
+    similarities, decisions = [], []
+    for qid in tqdm(utils.QUERIES.keys()):
+        similarity, decision = compare_community_embedding_with_query(
+            qid=qid,
+            embedding_df=embedding_df,
+        )
+        if similarity is not None and decision is not None:
+            similarities.extend(similarity)
+            decisions.extend(decision)
+    similarities = np.array(similarities, dtype=np.float32)
+    decisions = np.array(decisions, dtype=np.float32)
+
+    np.save("similarities.npy", similarities, allow_pickle=False)
+    np.save("decisions.npy", decisions, allow_pickle=False)
+
+    similarities = np.load("similarities.npy", allow_pickle=False)
+    decisions = np.load("decisions.npy", allow_pickle=False)
+
+    plot_community_decision(
+        similarities=similarities,
+        decisions=decisions,
+        filename=PLOT_DIR / f"community_decision_similarity.jpg",
+        title="Cosine similarity between\n query and community embeddings",
+        scale_width=True,
+        add_p_value=True,
+    )
+
+    print(f"saved plots to {PLOT_DIR}")
+
+
+if __name__ == "__main__":
+    main()
--- examples_notebooks/compute_community_embedding.py
+++ examples_notebooks/compute_community_embedding.py
@@ -2,19 +2,24 @@
 
 import asyncio
 import os
-import uuid
-from dataclasses import dataclass
+from pathlib import Path
 
+import numpy as np
+import pandas as pd
 from graphrag.model.text_unit import TextUnit
 from graphrag.query.llm.base import BaseTextEmbedding
 from graphrag.query.llm.oai.embedding import OpenAIEmbedding
 from graphrag.query.llm.oai.typing import OpenaiApiType
 from tqdm.asyncio import tqdm_asyncio
 
+INPUT_DIR = "./inputs/podcast"
+
 
 class TextEmbedder:
     def __init__(
-        self, text_embedder: BaseTextEmbedding, concurrent_coroutines: int = 32
+        self,
+        text_embedder: BaseTextEmbedding,
+        concurrent_coroutines: int = 32,
     ):
         self.text_embedder = text_embedder
         self.semaphore = asyncio.Semaphore(concurrent_coroutines)
@@ -46,16 +51,28 @@ class TextEmbedder:
             results.extend(embeddings)
         return results
 
+    async def aembed_raw_text_batch(
+        self, text: list[str], batch_size: int | None = 1000
+    ) -> list[TextUnit]:
+        if batch_size is None:
+            batch_size = len(text)
 
-if __name__ == "__main__":
+        results: list[TextUnit] = []
+        for i in range(0, len(text), batch_size):
+            batch = text[i : i + batch_size] if i + batch_size < len(text) else text[i:]
+            embeddings = await tqdm_asyncio.gather(
+                *[self.aembed_raw_text(input) for input in batch]
+            )
+            results.extend(embeddings)
+        return results
+
+
+def get_text_embedder() -> TextEmbedder:
     api_key = os.getenv("GRAPHRAG_OPENAI_API_KEY")
     api_base = os.getenv("GRAPHRAG_OPENAI_API_BASE")
     api_version = "2024-02-15-preview"
-    llm_model = "gpt-4o"
-    llm_deployment_name = "gpt-4o"
     embedding_model = "text-embedding-ada-002"
     embedding_deployment_name = embedding_model
-
     oai_embedder = OpenAIEmbedding(
         api_base=api_base,
         api_key=api_key,
@@ -65,5 +82,32 @@ if __name__ == "__main__":
         api_version=api_version,
         max_retries=50,
     )
+    return TextEmbedder(text_embedder=oai_embedder, concurrent_coroutines=32)
 
-    text_embedder = TextEmbedder(text_embedder=oai_embedder, concurrent_coroutines=32)
+
+def embed_text(text: str) -> np.ndarray:
+    text_embedder = get_text_embedder()
+    embedding = asyncio.run(text_embedder.aembed_raw_text(text))
+    return np.array(embedding, dtype=np.float32)
+
+
+def extract_community_embeddings():
+    text_embedder = get_text_embedder()
+
+    report_df = pd.read_parquet(f"{INPUT_DIR}/create_final_community_reports.parquet")
+
+    communities = report_df.community.to_list()
+    full_contents = report_df.full_content.to_list()
+
+    embeddings = asyncio.run(text_embedder.aembed_raw_text_batch(full_contents))
+
+    embedding_df = pd.DataFrame({"community": communities, "embedding": embeddings})
+
+    filename = Path("inputs/podcast/community_embedding.parquet")
+    embedding_df.to_parquet(filename)
+
+    print(f"saved community embeddings to {filename}")
+
+
+if __name__ == "__main__":
+    extract_community_embeddings()
